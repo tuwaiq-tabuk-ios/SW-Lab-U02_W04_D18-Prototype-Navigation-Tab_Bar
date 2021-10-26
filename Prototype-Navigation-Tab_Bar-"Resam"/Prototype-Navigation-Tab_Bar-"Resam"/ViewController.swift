304455bb62f0986189a48a1db8cca4006d20b2aa
Ressam Al-Thebailah <ralthebailah@MacBook-Air-Ressam.local>
2021-10-26 21:54:26 +0300

    Completed

---
+++ Prototype-Navigation-Tab_Bar-"Resam"/Prototype-Navigation-Tab_Bar-"Resam"/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype-Navigation-Tab_Bar-"Resam"
+//
+//  Created by Ressam Al-Thebailah on 20/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
