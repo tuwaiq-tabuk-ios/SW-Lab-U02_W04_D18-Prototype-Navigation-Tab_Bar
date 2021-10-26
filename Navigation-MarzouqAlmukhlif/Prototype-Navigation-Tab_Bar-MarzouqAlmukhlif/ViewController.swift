ccef8bf2960bac3908fa8dba25f5a0b4d4738216
Marzouq Almukhlif <mhster_nice@Marzouqs-MacBook-Pro.local>
2021-10-26 13:49:48 +0300

    Finish My app

---
+++ Navigation-MarzouqAlmukhlif/Prototype-Navigation-Tab_Bar-MarzouqAlmukhlif/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation-MarzouqAlmukhlif
+//
+//  Created by Marzouq Almukhlif on 20/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
